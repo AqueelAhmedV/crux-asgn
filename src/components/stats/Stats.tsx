a6b5d33d15bdc857cfceaea00fd41ac851076eff
Aqueel Ahmed V <aqueelvallanchira13@gmail.com>
2024-02-26 23:31:28 +0530

    final

--- src/components/stats/Stats.tsx
+++ src/components/stats/Stats.tsx
@@ -1,19 +1,22 @@
+import { useWidgetContext } from "../../contexts/widget/themeContext"
 
 
 export function Stats({ statsData }: StatsProps) {
 
     const colors = ["#00ee00", "#ff8f00", 
     "#00897b", "#1e88e5", "#d81b60", "#f41"]
+
+    const { widgetTheme } = useWidgetContext()
     return (
         <div className="flex flex-col justify-around">
-            { statsData.map((s, i) => (<div className="border-l-4 " style={{ borderColor: colors[i%colors.length] }}>
-                    <span className="text-sm font-medium text-slate-600">
+            { statsData.map((s, i) => (<div className="border-l-4 flex flex-col gap-2 items-baseline justify-center" style={{ borderColor: colors[i%colors.length] }}>
+                    <span color={widgetTheme.content.focus} className="capitalize text-sm font-medium">
                         {s.label}
                     </span>
-                    <span className="text-xs text-slate-400">
+                    <span color={widgetTheme.content.color} className="text-xs text-slate-400">
                         {s.value}
                     </span>
-                    </div>)
+                </div>)
                 )
             }
         </div>
